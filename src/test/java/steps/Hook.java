b2236ccffe81d7e26e8551c79cc3cd86db6c7bff
vrvenkygcp <41734343+vrvenkygcp@users.noreply.github.com>
2018-07-31 20:40:23 +0530

    Updated path for ChromeDriver
    Updated path for ChromeDriver

--- src/test/java/steps/Hook.java
+++ src/test/java/steps/Hook.java
@@ -29,7 +29,7 @@ public class Hook extends BaseUtil{
 
 
         //Chrome driver
-        System.setProperty("webdriver.chrome.driver", "C:\\Users\\vrraghu\\ChromeDriver");
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\vrraghu\\ChromeDriver\\chromedriver.exe");
         base.Driver = new ChromeDriver();
     }
 
